f191e4f7485ab7e599dc347d04f38872fac472b6
byter0t <removable_substance367@passmail.com>
2025-08-17 17:38:33 +0100

    Update dir-setup.sh for jellyfin-stack

--- jellyfin-stack/dir-setup.sh
+++ jellyfin-stack/dir-setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Create Jellyfin directory; get .env template file and docker-compose
+# Create Jellyfin directory
 mkdir -p $HOME/docker/jellyfin-stack/jellyfin
-wget https://raw.githubusercontent.com/byter0t/docker-tutorials/refs/heads/main/jellyfin-stack/.env
-wget https://raw.githubusercontent.com/byter0t/docker-tutorials/refs/heads/main/jellyfin-stack/docker-compose.yml
 
 # Create directories for Jellyfin add-ons
 mkdir -p $HOME/docker/jellyfin-stack/wizarr # User sign up management
 mkdir -p $HOME/docker/jellyfin-stack/jellystat # Jellyfin stats
 mkdir -p $HOME/docker/jellyfin-stack/jellyseerr # Jellyfin requests
 
-# Move to newly created media management directory
+# Move to newly created media management directory; get .env template file and docker-compose
 cd $HOME/docker/jellyfin-stack
+wget https://raw.githubusercontent.com/byter0t/docker-tutorials/refs/heads/main/jellyfin-stack/.env
+wget https://raw.githubusercontent.com/byter0t/docker-tutorials/refs/heads/main/jellyfin-stack/docker-compose.yml
